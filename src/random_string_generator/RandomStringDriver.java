f5be5fb1c2c05c7eb8fc49967e2d0efae1b8aeab
Mayer Greenberg <mgreenbe17@student.touro.edu>
2019-02-16 23:49:48 -0500

    changed javadoc

--- src/random_string_generator/RandomStringDriver.java
+++ src/random_string_generator/RandomStringDriver.java
@@ -1,16 +1,34 @@
 package random_string_generator;
 
+import java.util.Date;
+
 public class RandomStringDriver {
 
 	public static void main(String[] args) {
-//		System.out.println(RandomStringTests.checkStringWithMethod(StringChecker::isLowerCaseWord, ""););
-//		System.out.println(RandomStringGenerator.upperCase(100));
-//		System.out.println(RandomStringGenerator.integer(100));
-//		System.out.println(RandomStringGenerator.allLetters(100));
-//		System.out.println(RandomStringGenerator.alphaNumeric(100));
-//		System.out.println(RandomStringGenerator.alphaNumericAndSpecialChars(100));
-//		System.out.println(RandomStringGenerator.specialChars(100));
-		//System.out.println(RandomStringGenerator.isInteger("35634"));
+		Date now = new Date();
+		int[] arr = new int[1];
+		for (int i = 0; i < arr.length; i++)
+			arr[i] = howManyTriesTilRandomString();
+		System.out.println("Total time" + (new Date().getTime() - now.getTime()) + " milliseconds");
+		 System.out.println(avg(arr));
 	}
-
+	public static int howManyTriesTilRandomString() {
+		Date now = new Date();
+		for (int i = 0; i != -1; i++) {
+			String x = RandomStringGenerator.lowerCase(2);
+			//System.out.println(i + ") " + x);
+			if (x.equals("av")) {
+				//System.out.println((new Date().getTime() - now.getTime()) + " milliseconds");
+				return i;//break;
+			}
+		}
+		
+		return 0;
+	}
+	public static double avg(int[] arr) {
+		double d = 0;
+		for(int x: arr)
+			d += x;
+		return d / arr.length;
+	}
 }
